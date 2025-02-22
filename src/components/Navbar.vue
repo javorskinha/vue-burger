7bac74f036a9aae807281e4f288d2a8cf7083237
javorskinha <andrielijavorski@gmail.com>
2025-02-22 20:08:07 -0300

    criando projeto

---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav id="nav">
+        <RouterLink to="/">
+            <img src="" alt="" id="logo">
+        </RouterLink>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+    </nav>
+</template>
+
+<script>
+    export default{
+        name: 'Navbar'
+    }
+</script>
+
+<style>
+
+</style>
+
+
