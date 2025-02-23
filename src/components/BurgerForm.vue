058846af64db282ddcd933cf2f4a3d405e165aea
javorskinha <andrielijavorski@gmail.com>
2025-02-23 12:19:45 -0300

    enviar dados para o servidor

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <p>componente mensagem</p>
+        <Mensagem :msg="msg" v-show="msg"/>
         <div>
-            <form id="burger-form">
+            <form id="burger-form" @submit="createBurger">
                 <div class="input-container">
                     <label for="nome">Nome do cliente:</label>
                     <input type="text" id="nome" name="nome" v-model="nome" placeholder="Digite seu nome">
@@ -11,21 +11,21 @@
                     <label for="pao">Escolha o pão:</label>
                     <select name="pao" id="pao" v-model="pao">
                         <option value="">Selecione</option>
-                        <option value="integral">Integral</option>
+                        <option v-for="pao in paes" :key="pao.id" :value="pao.tipo">{{ pao.tipo }}</option>
                     </select>
                 </div>
                 <div class="input-container" id="opcionais-container">
                     <label for="carne">Escolha sua proteiína:</label>
                     <select name="carne" id="carne" v-model="carne">
                         <option value="">Selecione</option>
-                        <option value="maminha">maminha</option>
+                        <option v-for="carne in carnes" :key="carne.id" :value="carne.tipo">{{ carne.tipo }}</option>
                     </select>
                 </div>
                 <div class="input-container">
                     <label for="opcionais"a id="opcionais-title">Selecione os opcionais:</label>
-                    <div id="checkbox-container">
-                        <input type="checkbox" name="opcionais" id="opcionais" v-model="opcionais" value="salame">
-                        <span class="span">Salame</span>
+                    <div id="checkbox-container" v-for="opcional in opcionaisData" :key="opcional.id">
+                        <input type="checkbox" name="opcionais" id="opcionais" v-model="opcionais" :value="opcional.tipo">
+                        <span class="span">{{ opcional.tipo }}</span>
                     </div>
                 </div>
                 <div class="input-container">
@@ -37,8 +37,70 @@
 </template>
 
 <script>
+import Mensagem from './Mensagem.vue';
+
     export default{
-        name: 'BurgerForm'
+        name: 'BurgerForm',
+        data() {
+            return {
+                paes: null,
+                carnes: null,
+                opcionaisData: null,
+                nome: null,
+                pao: null,
+                carne: null,
+                opcionais: [],
+                msg: null
+            }
+        },
+        methods: {
+            async getIngredientes (){
+                const req = await fetch('http://localhost:3000/ingredientes');
+                const data = await req.json();
+
+                this.paes = data.paes;
+                this.carnes = data.carnes;
+                this.opcionaisData = data.opcionais;
+            },
+            async createBurger(e){
+                e.preventDefault();
+                const data = {
+                    nome: this.nome,
+                    pao: this.pao,
+                    carne: this.carne,
+                    opcionais: Array.from(this.opcionais),
+                    status: 'Solicitado'
+
+                }
+
+                const dataJson = JSON.stringify(data);
+
+                const req = await fetch('http://localhost:3000/burgers', {
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/json'},
+                    body: dataJson
+                });
+
+                const res = await req.json();
+
+                //msg de sistema
+                this.msg = `Pedido N ${res.id} realizado com sucesso`;
+
+                //lipar msg
+                setTimeout(() => this.msg ='', 2000)
+
+                this.nome = '';
+                this.pao = '';
+                this.carne = '';
+                this.opcionais = ';'
+            }
+        },
+        mounted (){
+            this.getIngredientes()
+        },
+        components: {
+            Mensagem
+        }
     }
 </script>
 
---
+++ src/components/Mensagem.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="mensagem-container">
+        <p>{{ msg }}</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'Mensagem',
+        props: {
+            msg: String
+        }
+    }
+
+</script>
+
+<style scoped>
+    .mensagem-container {
+        color: #004085;
+        background-color: #cce5ff;
+        border: 2px solid #b8daff;
+        border-radius: 5px;
+        padding: 10px;
+        max-width: 400px;
+        margin: 30px auto;
+    }
+</style>
+
+
+
